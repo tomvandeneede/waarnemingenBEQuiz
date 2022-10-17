8137dbe4e5748fc27285f1bd8497c80eeb9e8aa6
tomvandeneede <t.vandeneede@pandora.be>
2022-10-17 11:15:44 +0200

    Added packagink scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
-git clone --branch master https://github.com/tomvandeneede/waarnemingenBEQuiz .
+@echo off
+rmdir /s build
+git clone --branch master https://github.com/tomvandeneede/waarnemingenBEQuiz build
 set PATH=%PATH%;c:\mingw\bin
 set GOARCH=386
 set GOOS=windows
 set CGO_ENABLED=1
-go build
+cd build
+go build -o ../Roofvogels.exe
+cd ..
