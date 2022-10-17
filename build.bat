f7c0cc9a959ed906abf4b4b30b2c2ada1bcf0f3e
tomvandeneede <t.vandeneede@pandora.be>
2022-10-17 11:43:18 +0200

    Added packaging script windows

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
 rmdir /s build
 git clone --branch master https://github.com/tomvandeneede/waarnemingenBEQuiz build
-set PATH=%PATH%;c:\mingw\bin
+set PATH=%PATH%;c:\mingw\bin;%userprofiel%\go\bin\windows_386
 set GOARCH=386
 set GOOS=windows
 set CGO_ENABLED=1
 cd build
-go build -o ../Roofvogels.exe
+rem go build -o ../Roofvogels.exe
+fyne package -os windows -icon icon.png
 cd ..
